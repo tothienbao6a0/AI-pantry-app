25989bd57c1c560886154c9f388b86f1a86cf0d8
Bao To <tothienbao6a0@gmail.com>
2024-07-31 02:29:45 +0700

    added

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,13 +1,27 @@
 "use client"
 import React, { useEffect, useState } from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography, Button, Modal } from "@mui/material";
 import {firestore} from '@/firebase'
 import {collection} from 'firebase/firestore'
 import {getDocs, query} from 'firebase/firestore'
 
-
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: "white",
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Home() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const [pantry, setPantry] = useState([])
   useEffect(() => {
     const updatePantry = async () => {
@@ -34,7 +48,29 @@ export default function Home() {
       alignItems={"center"}
       
       flexDirection={"column"}
+      gap={2}
     >
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+      <Button 
+        variant="contained" onClick={handleOpen}
+      >
+
+        Add
+      </Button>
       <Box border={"1px solid black"}
         width="800px"
         height="100px"
